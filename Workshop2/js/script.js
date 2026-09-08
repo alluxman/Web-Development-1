a9c74ae25121bc827788b1bd853ea6837efb6c17
alluxman <alexandra.manieva@student.laurea.fi>
2026-09-08 18:35:30 +0300

    WS2 done I think

---
+++ Workshop2/js/script.js
@@ -0,0 +1,45 @@
+// STEP 1: Print text to the browser console is string ""
+
+console.log("Hello, World");
+
+// STEP 2: Variables
+
+const userName = "Aksu";
+const favoriteAnimal = "Frenchie Doggo";
+
+// Store text values in variables
+
+// Print the variables to the console
+
+console.log("My name is " + userName);
+console.log("My favorite animal is " + favoriteAnimal);
+
+// STEP 3: User interaction
+
+// Display a pop-up message
+
+alert("Welcome to the Frenchie Doggo page!");
+
+// Ask the user for their name
+
+const visitorName = prompt("What is your name?");
+
+// Print the user's answer
+
+console.log(visitorName);
+
+// Create a greeting using the user's answer
+
+console.log("Hello, " + visitorName + "! Welcome to the Frenchie Doggo page!");
+
+// Ask the user for their favorite animal
+
+const visitorFavoritAnimal = prompt("What is your favorite animal?");
+
+// Create a sentence using both answers
+
+console.log("Oh, " + visitorName + "! That is awesome that " + visitorFavoritAnimal + " is your fav animal, cool!");
+
+
+// ⭐⭐ BONUS Ask the user for their favorite animal.If their favorite animal is "Blue Elephant", print: Great choice! That's my favorite animal too!
+// ⭐⭐ BONUS Otherwise, print: Nice! Your favorite animal is [animal]. Can you figure out how to do this using if / else?
